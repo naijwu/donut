5a3f2b7f703192941ecc9e7567a04aedf66cdd62
DonaldKLee <Donald3010@outlook.com>
2024-08-01 14:08:19 -0700

    merge

--- frontend/src/screens/DonutChat/DonutChat.tsx
+++ frontend/src/screens/DonutChat/DonutChat.tsx
@@ -5,6 +5,14 @@ export default function DonutChatScreen() {
     return (
         <div>
             donut chatroom
+            {/* Profile Images */}
+            {/* Donut Name - Edit*/}
+            {/* Text */}
+            {/* Chat Messages */}
+            {/* Send Message Input */}
+            <div>
+                <input className={styles.chatBoxInput} placeholder='Send Message...' />
+            </div>
         </div>
     )
 }
