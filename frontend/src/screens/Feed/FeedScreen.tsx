4ff1dd99c800e8cd6dc01ba7239e8fcba2d8e341
DonaldKLee <Donald3010@outlook.com>
2024-08-05 05:17:08 -0700

    search frontend

--- frontend/src/screens/Feed/FeedScreen.tsx
+++ frontend/src/screens/Feed/FeedScreen.tsx
@@ -1,32 +1,64 @@
+import { useState, useEffect } from 'react';
 import DonutCard from "@/components/DonutCard/DonutCard";
 import { Title } from "@/components/Typography/Typography";
 import { Donut } from "@/lib/types";
 import { useAuthContext } from "@/utility/Auth";
-
-import styles from './FeedScreen.module.css'
+import styles from './FeedScreen.module.css';
+import axios from 'axios';
 
 export default function FeedScreen({
-    donuts
+    donuts: initialDonuts
 }: {
     donuts: Donut[]
 }) {
-
     const { user } = useAuthContext();
+    const [searchTerm, setSearchTerm] = useState('');
+    const [filteredDonuts, setFilteredDonuts] = useState(initialDonuts);
+
+    useEffect(() => {
+        const fetchFilteredDonuts = async () => {
+            try {
+                // TODO: MAKE API
+                const res = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/search/${searchTerm}`, {
+                    withCredentials: true
+                });
+                setFilteredDonuts(res.data);
+            } catch (err) {
+                console.error("Failed to fetch filtered donuts:", err);
+            }
+        };
+
+        if (searchTerm) {
+            fetchFilteredDonuts();
+        } else {
+            setFilteredDonuts(initialDonuts);
+        }
+    }, [searchTerm, initialDonuts]);
+
+    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(event.target.value);
+    };
 
     return (
         <div className={styles.wrapper}>
             <Title>
-                Welcome,<br/>{user?.name}
+                Welcome,<br />{user?.name}
             </Title>
-            {
-                donuts ? 
+            <input
+                className={styles.searchInput}
+                type="text"
+                placeholder="Search Posts..."
+                value={searchTerm}
+                onChange={handleSearchChange}
+            />
+            { filteredDonuts ? 
                 <div className={styles.container}>
-                    {donuts?.map((d,i) => (
+                    {filteredDonuts.map((d, i) => (
                         <DonutCard key={i} data={d} />
                     ))}
                 </div> :
                 <div>No Donuts Available</div>
             }
         </div>
-    )
+    );
 }
